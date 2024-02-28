26c6cd1fa926bc19aed2528996b84624120af639
Takunda Hwengwere <munashwengz@gmail.com>
2024-02-28 14:16:06 +0200

    dashboard

--- Site1.Master.designer.cs
+++ Site1.Master.designer.cs
@@ -33,15 +33,6 @@ namespace Grit_Management_System
         protected global::System.Web.UI.HtmlControls.HtmlLink pagestyle;
 
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
